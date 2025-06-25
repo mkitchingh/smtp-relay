bdefee7c831e1af7a1fb46e57b1389748e5f3af3
Matthew Kitchin <25916510+mkitchingh@users.noreply.github.com>
2025-06-25 12:17:49 -0500

    Add files via upload

--- src/SmtpRelay/Program.cs
+++ src/SmtpRelay/Program.cs
@@ -1,1 +1,16 @@
-// Program entry point
+using System.ServiceProcess;
+
+namespace SmtpRelay
+{
+    static class Program
+    {
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun = new ServiceBase[]
+            {
+                new RelayService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
--- src/SmtpRelay/RelayService.cs
+++ src/SmtpRelay/RelayService.cs
@@ -1,1 +1,18 @@
-// Windows Service logic
+using System.ServiceProcess;
+using System.Diagnostics;
+
+namespace SmtpRelay
+{
+    public class RelayService : ServiceBase
+    {
+        protected override void OnStart(string[] args)
+        {
+            EventLog.WriteEntry("SmtpRelay", "Service started.");
+        }
+
+        protected override void OnStop()
+        {
+            EventLog.WriteEntry("SmtpRelay", "Service stopped.");
+        }
+    }
+}
